211c2b133cb2ad575ea49b327a1c841dcb3d7466
bollylu <lbolly@sharenet.be>
2016-11-14 10:48:39 +0100

    x

---
+++ Debug/Thermometre.cpp.d
@@ -0,0 +1,16 @@
+C:\Users\bollylu.NEWNET\AppData\Local\Temp\VMicroBuilds\LaserControl\uno\Thermometre.cpp.o: \
+ C:\Users\bollylu.NEWNET\AppData\Local\Temp\VMicroBuilds\LaserControl\uno\Thermometre.cpp \
+ C:\Users\bollylu.NEWNET\AppData\Local\Temp\VMicroBuilds\LaserControl\uno\Thermometre.h \
+ c:\dev.arduino\libraries\OneWire/OneWire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ c:\dev.arduino\libraries\DallasTemperature/DallasTemperature.h
